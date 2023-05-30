a3e64227b3b8f56ce8b0bd32ee4aec4acb3d34e0
HubertNastaly <h.nastaly@gmail.com>
2023-05-30 15:34:08 +0200

    Rename GemColor type to Color

--- src/types/color.ts
+++ src/types/color.ts
@@ -1,7 +1,7 @@
 export const COLORS = ['white', 'blue', 'green', 'red', 'black'] as const
 
 export type BasicColor = typeof COLORS[number]
-export type GemColor = BasicColor | 'gold'
+export type Color = BasicColor | 'gold'
 
 export type Tokens = Record<BasicColor, number>
 
