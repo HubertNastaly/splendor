0578dbca16edde4d3555e0005a9d5b3abef55806
HubertNastaly <h.nastaly@gmail.com>
2023-06-02 17:47:43 +0200

    Rename reducer to gameReducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { reducer } from './reducer'
+import { gameReducer } from './reducers'
 import { getDefaultState } from './defaultState'
 import { mockState } from '@/mocks'
 
 export const store = configureStore({
   preloadedState: import.meta.env.VITE_MODE === 'dev' ? mockState() : getDefaultState(),
-  reducer
+  reducer: gameReducer
 })
