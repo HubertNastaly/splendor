e1c7fc2cc5df5aa2493e30520f9e4f43463eb9db
HubertNastaly <h.nastaly@gmail.com>
2023-06-05 09:08:41 +0200

    Display reserved cards

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const MAX_PLAYERS_NUMBER = 4
 export const CARDS_PER_LEVEL = 4
 export const MIN_BANK_TOKENS_TO_PICK_TWO = 4
 export const MAX_TOKENS_LIMIT = 10
+export const MAX_RESERVED_CARDS_LIMIT = 3
 export const DEFAULT_NAMES = ['Adam', 'Bob', 'Cindy']
 
 export const TOKEN_SIZE = 72
